c40678eed493ab495b5f4f1904ce03bfe518f1ee
Ansuhan <anushan559@gmail.com>
2024-06-15 21:10:16 +0530

    commited for user and admindashboard

--- src/Routers.js
+++ src/Routers.js
@@ -1,9 +1,13 @@
-import React from "react";
+import {React,Fragment} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./core/Home";
 import Cart from "./core/Cart"
 import Signup from "./user/Signup";
 import Sigin from "./user/Signin"
+import UserDashboard from "./user/UserDashboard"
+import AdminDashboard from "./user/AdminDashBoard"
+ 
 const Routers = () => {
   return (
     <BrowserRouter>
@@ -12,9 +16,13 @@ const Routers = () => {
         <Route  path="/cart" element={<Cart/>} />
         <Route  path="/Signup" element={<Signup/>} />
         <Route  path="/Sigin" element={<Sigin/>} />
+        <Route path="/user/dashboard" element={<UserDashboard/>} />
+        <Route path="/admin/dashboard" element={<AdminDashboard/>} />
         
     </Routes>
+    
     </BrowserRouter>
+    
   );
 };
 
